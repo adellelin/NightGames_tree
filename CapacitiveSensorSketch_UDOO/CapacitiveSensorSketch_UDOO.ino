2e3c3d076cdcf529b114cc4b2d3ac62b91244b30
ADELF <adelle.lin@gmail.com>
2015-04-09 22:39:38 -0400

    Added second string of data

--- CapacitiveSensorSketch_UDOO/CapacitiveSensorSketch_UDOO.ino
+++ CapacitiveSensorSketch_UDOO/CapacitiveSensorSketch_UDOO.ino
@@ -10,45 +10,56 @@
 
 
 CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
-//CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
-
-const int ELwire = 3;
+CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
+//CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+int ELwire1 = 3;
+int ELwire2 = 9;
 
 void setup()                    
 {
-  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
-   Serial1.begin(9600);
-   
-  // setup ELwire
-  pinMode(ELwire, OUTPUT);
-  digitalWrite(ELwire, LOW);
+   //cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   Serial1.begin(2400);
+   pinMode(ELwire1, OUTPUT);
+   digitalWrite(ELwire1, LOW);
+   pinMode(ELwire2, OUTPUT);
+   digitalWrite(ELwire2, LOW);
 }
 
 void loop()                    
 {
     long start = millis();
     long total1 =  cs_4_2.capacitiveSensor(30);
-    //long total2 =  cs_4_6.capacitiveSensor(30);
-
+    long total2 =  cs_4_6.capacitiveSensor(30);
+//    long total3 =  cs_4_8.capacitiveSensor(30);
 
     Serial1.print(millis() - start);        // check on performance in milliseconds
     Serial1.print("\t");                    // tab character for debug windown spacing
 
     Serial1.print(total1);                  // print sensor output 1
+    Serial1.print("\t");
+    Serial1.print(total2);                  // print sensor output 2
     Serial1.println("\t");
-//    Serial.print(total2);                  // print sensor output 2
-//    Serial.println("\t");
-//    Serial.println("\n");
+//    Serial1.println(total3);                // print sensor output 3
 
-    
+    delay(1);                             // arbitrary delay to limit data to Serial1 port 
+
     if(total1 < 500) {
-      digitalWrite(ELwire, LOW);
+      digitalWrite(ELwire1, LOW);
     } else {
-      digitalWrite(ELwire, HIGH);
-      delay(1000);
-      digitalWrite(ELwire,LOW);
-      delay(2000);
-    }     
-   
-    delay(100);                             // arbitrary delay to limit data to serial port 
+      digitalWrite(ELwire1, HIGH);
+//      delay(1000);
+//      digitalWrite(ELwire1,LOW);
+//      delay(2000);
+    }  
+    
+    
+    if(total2 < 500) {
+      digitalWrite(ELwire2, LOW);
+    } else {
+      digitalWrite(ELwire2, HIGH);
+//      delay(1000);
+//      digitalWrite(ELwire2,LOW);
+//      delay(2000);
+    }  
+
 }
